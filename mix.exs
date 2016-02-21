fb2c77cfb87212863ef2cefca064724e91ce7d6f
Sascha Brink <sascha.brink@gmail.com>
2016-02-21 18:11:28 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixSimpleForm.Mixfile do
 
   def project do
     [app: :phoenix_simple_form,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
